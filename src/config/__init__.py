f25c92dbcf5a034495b2183cdaa3569031c0be18
Rutvej Waghela <rutvej.waghela@routemobile.com>
2021-11-11 19:04:47 +0530

    remove header

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -1,6 +1,5 @@
 MESSAGE_PAYLOAD = { "phone": "",
                     "media": {"type": "interactive_reply",
-                            "header": {"text": "Consent" },
                                             "body": "HI {}, Do you Want to Exchange visiting card with {}", "footer_text": "c@2021", "button": [ { "id": "56", "title": "Yes" }, { "id": "55", "title": "No" } ] } }
 
 
