fc6075ae2150e3715cfeae6b7a798d961eda4976
Gaurav Modi <gmodi94@users.noreply.github.com>
2021-11-12 16:58:13 +0530

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from src.main import app
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://rduyhaijhswewi:ae48d038f00869949241ae4aae2cde0896d47a8f40da61fc529f5c06504e3d95@ec2-107-20-127-127.compute-1.amazonaws.com:5432/dftfh0bgikn7rr"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://loudtsfzmqobmn:36a3d46f18640439a1bbed8dbd730a29a7fc97852130b59f1fb4ea846a662925@ec2-107-23-213-65.compute-1.amazonaws.com:5432/ddn63kvo42m0er"
 db = SQLAlchemy(app) 
 
