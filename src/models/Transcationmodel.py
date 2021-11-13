56ef4971f2266260b14ac1e471a1ef4fa39869ab
Rutvej Waghela <rutvej.waghela@routemobile.com>
2021-11-13 21:10:40 +0530

    model change

--- src/models/Transcationmodel.py
+++ src/models/Transcationmodel.py
@@ -2,8 +2,8 @@ from src.models import db
 
 class transcation_details(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    from_id = db.Column(db.String,nullable=True)
-    to_id = db.Column(db.String)
-    status = db.Column(db.String)
+    from_id = db.Column(db.String(50),nullable=True)
+    to_id = db.Column(db.String(50))
+    status = db.Column(db.String(50))
     created_on = db.Column(db.DateTime, server_default=db.func.now())
     updated_on = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
--- src/models/otpdetails.py
+++ src/models/otpdetails.py
@@ -2,5 +2,5 @@ from src.models import db
 
 class otp_details(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    otp_data = db.Column(db.String,nullable=True)
+    otp_data = db.Column(db.String(50),nullable=True)
     mobile_number = db.Column(db.String(50))
