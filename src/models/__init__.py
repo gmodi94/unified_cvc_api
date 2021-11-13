d745957c93b4746bde54d0cf5b998760023531cd
Rutvej Waghela <rutvej.waghela@routemobile.com>
2021-11-13 16:35:49 +0530

    db change

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from src.main import app
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://loudtsfzmqobmn:36a3d46f18640439a1bbed8dbd730a29a7fc97852130b59f1fb4ea846a662925@ec2-107-23-213-65.compute-1.amazonaws.com:5432/ddn63kvo42m0er"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/rapid"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://loudtsfzmqobmn:36a3d46f18640439a1bbed8dbd730a29a7fc97852130b59f1fb4ea846a662925@ec2-107-23-213-65.compute-1.amazonaws.com:5432/ddn63kvo42m0er"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/rapid"
 db = SQLAlchemy(app) 
 
