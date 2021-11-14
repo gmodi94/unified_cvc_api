045029caba661bc4f05f8936942c1be60a6a07a6
Rutvej Waghela <rutvej.waghela@routemobile.com>
2021-11-14 13:04:30 +0530

    removed address

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -58,8 +58,7 @@ def csv_to_base64(csvdata):
     return data
 
 def user_to_vcard(user):
-    vcardformat = "BEGIN:VCARD\nVERSION:2.1\nN:{};;;;\nFN:{}\nTEL;HOME:{}\nEMAIL:{}\nADR;HOME:;;{}\nEND:VCARD"
-    vcardformat.format(user.first_name,user.first_name+" "+user.last_name,user.email,user.mobile_number,user.address)
+    vcardformat = "BEGIN:VCARD\nVERSION:2.1\nN:{};;;;\nFN:{}\nTEL;HOME:{}\nEMAIL:{}\nEND:VCARD".format(user.first_name,user.first_name+" "+user.last_name,user.mobile_number,user.email)
     f = io.StringIO()
     f.write(vcardformat)
     data = base64.b64encode(f.getvalue().encode()).decode()
